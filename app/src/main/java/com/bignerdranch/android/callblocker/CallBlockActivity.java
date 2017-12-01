56c8afd76c4d88340c6cefdaa38bbe578c5dcc34
crifa001 <crifa001@rangers.uwp.edu>
2017-11-30 18:20:16 -0600

    Added ability to listen for calls

--- app/src/main/java/com/bignerdranch/android/callblocker/CallBlockActivity.java
+++ app/src/main/java/com/bignerdranch/android/callblocker/CallBlockActivity.java
@@ -1,6 +1,10 @@
 package com.bignerdranch.android.callblocker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,8 @@ public class CallBlockActivity extends AppCompatActivity {
 
     private Button showListButton;
     private Button addToListButton;
+    public final static int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 11;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,34 @@ public class CallBlockActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), BlackListActivity.class));
             }
         });
-    }
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+    }
+}
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                }
+                return;
+            }
+        }
+    }
 }
--- app/src/main/java/com/bignerdranch/android/callblocker/CallReceiver.java
+++ app/src/main/java/com/bignerdranch/android/callblocker/CallReceiver.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import com.android.internal.telephony.ITelephony;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 
@@ -19,40 +19,49 @@ public class CallReceiver extends BroadcastReceiver {
     private String number;
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Received a Broadcast");
         // If, the received action is not a type of "Phone_State", ignore it
-        if (intent.getAction().equals("android.intent.action.PHONE_STATE"))
-            // Fetch the number of incoming call
-            number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        String action = intent.getAction();
+        if (action != null) {
+            if (action.equals("android.intent.action.PHONE_STATE"))
+                // Fetch the number of incoming call
+                number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
             Blacklist blacklist = new Blacklist();
             blacklist.setPhoneNumber(number);
-        // Check, whether this is a member of "Black listed" phone numbers stored in the database
-        if(BlackListActivity.blockList.contains(blacklist)) {
-            // If yes, invoke the method
-            disconnectPhoneItelephony(context);
-            return;
-        }
-        else{
-            Log.d(TAG, "Not a Telephone Call");
+            // Check, whether this is a member of "Black listed" phone numbers stored in the database
+            if(BlackListActivity.blockList.contains(blacklist)) {
+                // If yes, invoke the method
+                disconnectPhoneItelephony(context);
+            }
+            else{
+                Log.d(TAG, "Not a Telephone Call");
             }
         }
+        }
+
     // Method to disconnect phone automatically and programmatically
     // Keep this method as it is
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private void disconnectPhoneItelephony(Context context)
     {
-        ITelephony telephonyService;
-        TelephonyManager telephony = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        try
-        {
-            Class c = Class.forName(telephony.getClass().getName());
+        TelephonyManager tm = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            Class c = Class.forName(tm.getClass().getName());
             Method m = c.getDeclaredMethod("getITelephony");
             m.setAccessible(true);
-            telephonyService = (ITelephony) m.invoke(telephony);
-            telephonyService.endCall();
-        }
-        catch (Exception e)
-        {
+            Object telephonyService = m.invoke(tm); // Get the internal ITelephony object
+            c = Class.forName(telephonyService.getClass().getName()); // Get its class
+            m = c.getDeclaredMethod("endCall"); // Get the "endCall()" method
+            m.setAccessible(true); // Make it accessible
+            m.invoke(telephonyService); // invoke endCall()
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
